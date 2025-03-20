841b1a78d9c6e714330c8ec932ccb86f8b6494e6
CJ <113473673+C0REC0N@users.noreply.github.com>
2025-03-20 17:18:53 +0000

    Update controller_controls.py

--- TIAGo/Manual/controller_controls.py
+++ TIAGo/Manual/controller_controls.py
@@ -3,7 +3,7 @@ import tkinter as tk
 def con_controls():   
     controls_window = tk.Toplevel()
     controls_window.title("Controller Controls")
-    controls_window.geometry("300x200")
+    controls_window.geometry("1000x1000")
     controls_window.configure(bg="white")
 
     label = tk.Label(controls_window, text="Controller Control Instructions", font=("Arial", 14), bg="white")
@@ -20,10 +20,12 @@ def con_controls():
     Right Bumper - Offer Hand
     Left Bumper - Retract Hand
     
-    DPad Up - Raise Torso
-    DPad Down - Lower Torso
+    DPad - Torso Up/Down
     
     A - Say Hello
+    B - Exit Program
+    Y - Return to Home
+    X - Cancel Movement
     """
     text_widget = tk.Label(controls_window, text=instructions, font=("Arial", 12), bg="white", justify="left")
     text_widget.pack(pady=10)
